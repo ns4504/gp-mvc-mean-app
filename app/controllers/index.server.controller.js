b0d09508720c8369824524ff6e8cb5ee5a59734b
Nathan04Scott <natsco9120@students.ecpi.edu>
2023-12-12 12:29:24 -0500

    I didn't realize there was a second half to this GP until now.

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,8 @@
 exports.render =
     function (req, res) {
-        res.status(200).send('Helo, Nathan Scott!');
+        if (req.session.lastVisit) {
+            console.log(req.session.lastVisit);
+        }
+        req.session.lastVisit = new Date();
+        res.render('index', { title: 'Hello, Nathan Scott' });
     };
